88bdc7aa978c9adc71d91e590f75b15a0eeddf9a
Susant <tester.susant@gmail.com>
2020-12-11 12:10:34 +0530

    This is my first commit

---
+++ src/test/java/selfreliant/AppiumTest.java
@@ -0,0 +1,18 @@
+package selfreliant;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAndroid()
+	{
+		System.out.println("NativeAndroid");
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+
+}
---
+++ src/test/java/selfreliant/RestAPITest.java
@@ -0,0 +1,18 @@
+package selfreliant;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("PostJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
